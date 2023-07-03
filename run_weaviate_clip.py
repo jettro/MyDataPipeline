9fc311a473aa42164801632daac665e12b1bb223
Jettro Coenradie <jettro@coenradie.com>
2023-07-03 10:58:17 +0200

    Add streamlit to the clip sample

--- run_weaviate_clip.py
+++ run_weaviate_clip.py
@@ -1,6 +1,8 @@
 import os
 import base64
 
+import streamlit as st
+
 from weaviatedb import WeaviateClient
 
 WEAVIATE_CLASS = "Toys"
@@ -33,21 +35,41 @@ def store_images(client: WeaviateClient) -> None:
                 )
 
 
-def query(client: WeaviateClient, query_text: str):
+def query(client: WeaviateClient, query_text: str, the_limit: int = 3):
     near_text = {"concepts": [query_text]}
 
     return (
         client.client.query
         .get(WEAVIATE_CLASS, ["filename"])
         .with_near_text(near_text)
-        .with_limit(5)
+        .with_limit(the_limit)
         .do()
     )
 
 
+def reload_data(client: WeaviateClient):
+    load_weaviate_schema(client=client, schema_path="./config_files/weaviate_clip.json")
+    store_images(client)
+
+
 if __name__ == '__main__':
     weaviate_client = WeaviateClient(overrule_weaviate_url="http://localhost:8080")
+    st.title("Search for toys")
 
-    # load_weaviate_schema(client=weaviate_client, schema_path="./config_files/weaviate_clip.json")
-    # store_images(weaviate_client)
-    print(query(weaviate_client, "traktor"))
+    st.sidebar.button(label="Reload data",
+                      key="reload_data",
+                      on_click=reload_data,
+                      kwargs={"client": weaviate_client})
+
+    n_cols = 3
+    n_picts = 6
+    n_rows = int(1 + n_picts // n_cols)
+    the_query = st.text_input('describe toy in one or a few words')
+    rows = [st.columns(n_cols) for _ in range(n_rows)]
+    cols = [column for row in rows for column in row]
+    if the_query:
+        response = query(client=weaviate_client, query_text=the_query, the_limit=n_picts)
+        if response["data"]["Get"]["Toys"]:
+            picts = [toy["filename"] for toy in response["data"]["Get"]["Toys"]]
+            for col, pict in zip(cols, picts):
+                col.image(image="./data_sources/images/" + pict, caption=pict, width=200)
